87dc281eace6103faf13d08bb84f7a573f231e70
440028392-oss <440028392@gapps.yrdsb.ca>
2026-04-10 18:08:00 +0000

    second

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ let sukHp =10
 let sukDmg = 0;
 let plyrHp = 5;
 let plyrDmg = 0;
+let dmg = 0;
+
 
 function updateHp()
 {
@@ -26,25 +28,39 @@ function log(message)
 
 
 
-function atk ()
+function atk (name, baseDmg)
 {
-  if (dmg === 0)
-  {
-   plyrDmg = Math.floor(Math.random()*5)+1
-  }
+  plyrDmg = Math.floor(Math.random()*5) + 1  
+  dmg =  baseDmg + plyrDmg
   if (dmg > sukHp)
   {
-    console.log("damage altered, orginal is " +plyrDmg)
-   plyrDmg = sukHp
+    console.log("damage altered, orginal is " + dmg)
+   dmg = sukHp
   }
   sukHp-=dmg
   updateHp()
-  log("Hit for " +plyrDmg + " damage!")
- plyrDmg = 0
+  log("Player used " + name + "!")
+  log("Hit for " + dmg + " damage!")
+ dmg = 0
   if (sukHp <= 0)
   {
     log("Sukuna died!")
     log("Game Over, you won!")
     sukHp+=10
   }
+}
+
+function endGame (message)
+{
+  let gameOverScreen = document.getElementById("gameOverScreen")
+
+  let msg=document.createElement("div")
+  msg.textContent = message
+
+  gameOverScreen.appendChild(msg)
+
+    setTimeout(() => 
+  {
+    msg.remove()
+  }, 5000)
 }
